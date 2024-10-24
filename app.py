3104450221a1a8633925412e53bb92cec9f6c908
yudairawan89 <yudairawan89@gmail.com>
2024-10-24 22:12:17 +0700

    app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import streamlit as st
 import pandas as pd
 import joblib
 from sklearn.preprocessing import StandardScaler
-from streamlit.runtime.legacy_caching import caching
-from streamlit_autorefresh import st_autorefresh
 
 # Menambahkan logo di sebelah kiri tulisan "UHTP Smart Fire Prediction"
 col1, col2 = st.columns([1, 6])  # Membuat layout kolom untuk logo dan judul
@@ -47,12 +45,9 @@ def load_scaler(scaler_path):
 # URL Data Google Sheets (format CSV)
 data_url = 'https://docs.google.com/spreadsheets/d/1ZscUJ6SLPIF33t8ikVHUmR68b-y3Q9_r_p9d2rDRMCM/export?format=csv'
 
-# Refresh otomatis setiap 3 detik
-st_autorefresh(interval=3000, key="data_refresh")
-
 # Tombol untuk refresh data manual
 if st.button('Refresh Data'):
-    caching.clear_cache()  # Hapus cache agar data terbaru dimuat
+    st.cache_data.clear()  # Hapus cache agar data terbaru dimuat
 
 # Deskripsi aplikasi yang dipindahkan ke bawah tombol
 st.markdown("""
@@ -122,15 +117,6 @@ if sensor_data is not None:
             # Menambahkan tampilan hasil prediksi di bawah data sensor
             st.subheader("Hasil Prediksi")
             st.dataframe(sensor_data)
-
-            # Fitur download hasil prediksi sebagai CSV
-            csv = sensor_data.to_csv(index=False)
-            st.download_button(
-                label="Download Hasil Prediksi sebagai CSV",
-                data=csv,
-                file_name='hasil_prediksi_kebakaran.csv',
-                mime='text/csv'
-            )
 
             # Tampilkan hasil prediksi data paling akhir setelah sensor data
             st.subheader("Hasil Prediksi Data Paling Akhir")
