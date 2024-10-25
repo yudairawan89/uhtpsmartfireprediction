0516bbeb65bf96aa765f369f699177416fc60584
yudairawan89 <yudairawan89@gmail.com>
2024-10-25 09:49:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import joblib
 from sklearn.preprocessing import StandardScaler
 import io
+import time
 
 # Fungsi untuk mengonversi hari ke bahasa Indonesia
 def convert_day_to_indonesian(day_name):
@@ -83,9 +84,8 @@ def load_scaler(scaler_path):
 # URL Data Google Sheets (format CSV)
 data_url = 'https://docs.google.com/spreadsheets/d/1ZscUJ6SLPIF33t8ikVHUmR68b-y3Q9_r_p9d2rDRMCM/export?format=csv'
 
-# Tombol untuk refresh data
-if st.button('Refresh Data'):
-    st.cache_data.clear()  # Hapus cache agar data terbaru dimuat
+# Auto-refresh setiap 3 detik
+st_autorefresh(interval=3000, key="data_refresh")
 
 # Muat Data
 sensor_data = load_data(data_url)
